ff0013fec52c383363e5209801f2fb05c10e052a
webcoderz <19884161+webcoderz@users.noreply.github.com>
2021-01-24 11:40:03 -0600

    Update loancalc.cpp

--- loancalc.cpp
+++ loancalc.cpp
@@ -11,12 +11,12 @@ int main(){
 	cout<<"Enter the yearly interest rate: ";
 	cin>>interest_rate;
 	cout<<endl;
-  cout<<"Enter the monthly payment: ";
+  	cout<<"Enter the monthly payment: ";
 	cin>>monthly_amount;
 	cout<<endl;
 
   month = 1;
-	monthly_interest_amount=(interest_rate/12);
+  monthly_interest_amount=(interest_rate/12);
   monthly_interest_cost = ((interest_rate/100.00)/12) * loan_amount;
   balance = loan_amount;
   cout<<"Month: " << month <<" | Initial loan amount($): "<<loan_amount<<" | Total interest($): "<<(loan_amount*(interest_rate/100.00))<<" | Monthly amount to be paid to interest($): "<<monthly_interest_cost<<" | Monthly interest(%): "<<monthly_interest_amount<<" | Balance($): "<<balance<<endl;
@@ -26,7 +26,7 @@ int main(){
     balance = balance - (monthly_amount-monthly_interest_cost);
     monthly_interest_cost = ((interest_rate/100.00)/12) * balance;
     month=month +1;
-	  cout<<"Month: " << month <<" | Initial loan amount($): "<<loan_amount<<" | Total interest($): "<<(loan_amount*(interest_rate/100.00))<<" | Monthly amount to be paid to interest($): "<<monthly_interest_cost<<" | Monthly interest(%): "<<monthly_interest_amount<<" | Balance($): "<<balance<<endl;
+    cout<<"Month: " << month <<" | Initial loan amount($): "<<loan_amount<<" | Total interest($): "<<(loan_amount*(interest_rate/100.00))<<" | Monthly amount to be paid to interest($): "<<monthly_interest_cost<<" | Monthly interest(%): "<<monthly_interest_amount<<" | Balance($): "<<balance<<endl;
     cout << fixed << showpoint << setprecision(2);
 
   }
